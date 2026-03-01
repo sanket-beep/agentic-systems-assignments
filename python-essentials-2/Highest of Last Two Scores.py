e8e8b5122e6b4a5a4e6f2a0cad482d9160efae30
sanket-beep <sanketkumar080@gmail.com>
2026-03-01 14:58:36 +0530

    second program of second assignment completed

---
+++ python-essentials-2/Highest of Last Two Scores.py
@@ -0,0 +1,16 @@
+class StudentScores:
+    def __init__(self, scores):
+        self.scores = scores
+
+    def highest_score(self):
+        if len(self.scores) < 2:
+            raise ValueError("Not enough scores to determine highest")
+        last_two_scores = self.scores[-2:]
+        return max(last_two_scores)
+    
+scores = input("Enter scores separated by space: ").split()
+scores = [float(score) for score in scores]
+
+student1 = StudentScores(scores)
+
+print(student1.highest_score())
